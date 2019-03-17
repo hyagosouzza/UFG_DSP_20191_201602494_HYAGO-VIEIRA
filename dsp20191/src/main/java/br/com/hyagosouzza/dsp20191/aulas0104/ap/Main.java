9c65966ad2eb9e15a8438f198ad138c6fd0fc188
Hyago Souza <hyagosouzza@hotmail.com>
2019-03-17 11:12:22 -0300

    recolocando os codigos com gitignore

---
+++ dsp20191/src/main/java/br/com/hyagosouzza/dsp20191/aulas0104/ap/Main.java
@@ -0,0 +1,24 @@
+package br.com.hyagosouzza.dsp20191.aulas0104.ap;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        //Primeira parte, trabalhando com arquivo .txt
+        Crud crud = new Crud("src/main/resources/samplefile.txt");
+        try {
+            crud.createFile();
+            System.out.println("Arquivo simplefile.txt criado!\n");
+            crud.writeFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Houve um erro ao criar o arquivo simplefile.txt");
+        }
+
+        //Segunda parte, arquivo csv
+        Csv csv = new Csv();
+        csv.run();
+    }
+}
